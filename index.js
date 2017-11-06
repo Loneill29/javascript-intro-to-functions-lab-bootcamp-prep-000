7e17a8c7864f3d26dfcb7cb54a9f091c061ab73a
Linda ONeill <oneill.linda21@gmail.com>
2017-11-06 20:11:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ function whisper(STRING)
 function logShout(string) {console.log('HELLO')}
 function logWhisper(string) {console.log('hello')}
 function sayHiToGrandma(string) {
- if (string.toLowerCase === string) {
- console.log("I can\'t hear you") }
-else if ( string.toUpperCase === STRING) {
-  console.log("YES INDEED!") }
-else {console.log("I love you, too") }
+if (string.toLowerCase() === string) return "I can\'t hear you!";
+else if (string === "I love you, Grandma.") return "I love you, too.";  
+else if ( string.toUpperCase() === string) return "YES INDEED!"   
 }
