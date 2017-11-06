a430dad047cac3cf38ed602cbcb7fd3883aca93c
Linda ONeill <oneill.linda21@gmail.com>
2017-11-06 20:10:13 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+function shout(string)
+{return string.toUpperCase()}
+function whisper(STRING)
+{return STRING.toLowerCase()}
+function logShout(string) {console.log('HELLO')}
+function logWhisper(string) {console.log('hello')}
+function sayHiToGrandma(string) {
+ if (string.toLowerCase === string) {
+ console.log("I can\'t hear you") }
+else if ( string.toUpperCase === STRING) {
+  console.log("YES INDEED!") }
+else {console.log("I love you, too") }
+}
